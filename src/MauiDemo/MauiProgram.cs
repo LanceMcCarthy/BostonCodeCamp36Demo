1fa459081c16f3f97aebe6f69252c28788676818
Lance McCarthy <LanceMcCarthy@users.noreply.github.com>
2024-03-21 14:05:40 -0400

    Update MauiProgram.cs

--- src/MauiDemo/MauiProgram.cs
+++ src/MauiDemo/MauiProgram.cs
@@ -1,18 +1,5 @@
 ﻿using Microsoft.Maui.LifecycleEvents;
 
-#if WINDOWS
-using WinUIEx;
-
-#elif MACCATALYST
-using CoreGraphics;
-using UIKit;
-
-#elif IOS
-#elif ANDROID
-#elif TIZEN
-
-#endif
-
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)] // For XamlC
 namespace MauiDemo;
 
@@ -29,37 +16,6 @@ public static class MauiProgram
                 fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                 fonts.AddFont("telerikfontexamples.ttf", "telerikfontexamples");
             });
-
-        // Needs using Microsoft.Maui.LifecycleEvents for this extension method
-        builder.ConfigureLifecycleEvents(events =>
-        {
-#if WINDOWS
-            // using WinUIEx
-            events.AddWindows(wndLifeCycleBuilder =>
-            {
-                wndLifeCycleBuilder.OnWindowCreated(window =>
-                {
-                    window.CenterOnScreen(1024,768); //uses WinUIEx
-                });
-            });
-
-#elif MACCATALYST
-
-                // Using CoreGraphics and UIKit
-                events.AddiOS(wndLifeCycleBuilder =>
-                {
-                    wndLifeCycleBuilder.SceneWillConnect((scene, session, options) =>
-                    {
-                        if (scene is UIWindowScene { SizeRestrictions: { } } windowScene)
-                        {
-                            windowScene.SizeRestrictions.MaximumSize = new CGSize(1200, 900);
-                            windowScene.SizeRestrictions.MinimumSize = new CGSize(600, 400);
-                        }
-                    });
-
-                });
-#endif
-        });
 
         return builder.Build();
     }
